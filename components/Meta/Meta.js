478c78099ad1a49d15d9dc4ba236ef9bf4c19adc
Matthew <matthewpham2012@gmail.com>
2022-05-20 09:27:58 -0700

    disable indexing

--- components/Meta/Meta.js
+++ components/Meta/Meta.js
@@ -10,6 +10,7 @@ function Meta(props) {
         name='keyword'
         content='Matthew, Pham, Front, End, Developer, UX, User, Experience, UI, Interface, Design, Portfolio, Web, San, Jose'
       />
+      <meta name="robots" content="noindex"/>
       <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
       <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
       <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
